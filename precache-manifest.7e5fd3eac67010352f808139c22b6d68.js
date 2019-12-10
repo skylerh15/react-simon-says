2674e7d4443c7f1461c331d178d10b16d6afe3ff
Skyler Hiscock <skylerhiscock@gmail.com>
2019-12-10 05:15:28 -0600

    Updates

---
+++ precache-manifest.7e5fd3eac67010352f808139c22b6d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb23963d8bff8f8b37cb2a010475b45c",
+    "url": "/react-simon-says/index.html"
+  },
+  {
+    "revision": "ba629c3a20a0519a3f5f",
+    "url": "/react-simon-says/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83b58b81a2069cc0ac47",
+    "url": "/react-simon-says/static/js/2.f9fd0892.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-simon-says/static/js/2.f9fd0892.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba629c3a20a0519a3f5f",
+    "url": "/react-simon-says/static/js/main.aad5762c.chunk.js"
+  },
+  {
+    "revision": "88af731bfe069248952b",
+    "url": "/react-simon-says/static/js/runtime-main.77baba63.js"
+  },
+  {
+    "revision": "9768527047bc0e7fc85a50f88777ff1b",
+    "url": "/react-simon-says/static/media/wood.97685270.jpg"
+  }
+]);
