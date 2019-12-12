46d0b0d3dcd38564116d3343966f04a3a3861808
Skyler Hiscock <skylerhiscock@gmail.com>
2019-12-12 07:20:02 -0600

    button will read start guessing if user can guess

--- src/app/views/board/board.tsx
+++ src/app/views/board/board.tsx
@@ -4,11 +4,22 @@ import SimonBoard from 'app/components/SimonBoard';
 import { useApp } from 'app/context/AppContext';
 
 const Board: FC = () => {
-    const { startGame, currentRound, canStartRound } = useApp();
+    const { startGame, currentRound, canStartRound, allowUserInput } = useApp();
+
+    const getButtonText = () => {
+        if (allowUserInput) {
+            return 'Start Guessing!';
+        }
+        if (canStartRound) {
+            return `Start ${currentRound ? `Round ${currentRound + 1}` : 'Game'}`;
+        }
+        return 'In Progress';
+    };
+
     return (
         <div>
             <button disabled={!canStartRound} style={{ marginBottom: '4px', width: '100%' }} onClick={startGame}>
-                {canStartRound ? `Start ${currentRound ? `Round ${currentRound + 1}` : 'Game'}` : 'In Progress'}
+                {getButtonText()}
             </button>
             <SimonBoard />
         </div>
