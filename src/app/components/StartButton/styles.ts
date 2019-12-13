ab705cb44a3b64e48620bb2d1a7478a9e126a739
Skyler Hiscock <skylerhiscock@gmail.com>
2019-12-13 01:19:05 -0600

    didn't need justify-self

--- src/app/components/StartButton/styles.ts
+++ src/app/components/StartButton/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const StyledStartButton = styled.button`
     height: 35px;
-    justify-self: center;
     width: 60%;
     margin-bottom: 10px;
 `;
